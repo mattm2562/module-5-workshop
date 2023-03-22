18b0b73d246482bf5390f8dd9b1d23f3ac7cd4e0
Matthew Mintun <matthewmintun1@gmail.com>
2023-03-22 18:35:28 -0400

    FizzBuzz algorithm created

--- FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzz.Core/FizzBuzzService.cs
@@ -1,5 +1,28 @@
 ﻿namespace FizzBuzz.Core;
 public class FizzBuzzService
 {
+    public string Print(int number)
+    {
+        if (IsDivisibleByThree(number) && IsDivisibleByFive(number)) {
+            return "FizzBuzz";
+        }
+        else if (IsDivisibleByFive(number)) {
+            return "Buzz";
+        }
+        else if (IsDivisibleByThree(number)) 
+        {
+            return "Fizz";
+        }
+        return number.ToString();
+    }
+
+    private bool IsDivisibleByThree (int number) {
+        return number % 3 == 0;
+    }
+
+    private bool IsDivisibleByFive (int number) {
+        return number % 5 == 0;
+    }
+
 
 }
